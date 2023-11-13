3e26610cb651c89f2a810fda7bef428d4d126f51
victoriaglavnik <92651835+victoriaglavnik@users.noreply.github.com>
2023-11-13 17:40:38 -0500

    Add files via upload

---
+++ ordermanager/ordermanager.js
@@ -0,0 +1,32 @@
+var http = require('OrderManager');
+
+class order {
+    constructor(date, product) {
+        this.date = date;
+        this.product = product;
+    }
+}
+class item {
+    constructor(name, price, size, quantity){
+        this.name = name;
+        this.price = price;
+        this.size = ("small", "medium", "large");
+        this.quantity = quantity;
+    }
+}
+const allorders = [
+    new order('2.2.23', 'coffee'),
+    new item('vanilla coffee', '12', 'medium', '1'),
+    new order('2.15.23', 'tea'),
+    new item('chi tea', '4', 'small', '1'),
+];
+
+exports.getRandomorder = () => {
+    return allorders[Math.floor(Math.random() * allorders.length)];
+}
+exports.getRandomitem = () => {
+    return allorders[Math.floor(Math.random() * allorders.length)];
+}
+exports.allorders = allorders;
+
+
